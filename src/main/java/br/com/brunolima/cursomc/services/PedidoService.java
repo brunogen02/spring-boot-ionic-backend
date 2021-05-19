227d4bd5af9a4e4fb4720e0a756be59c229088cd
Bruno Lima <brunogen02@gmail.com>
2021-05-19 19:38:04 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/br/com/brunolima/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package br.com.brunolima.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.brunolima.cursomc.domain.Pedido;
+import br.com.brunolima.cursomc.repositories.PedidoRepository;
+import br.com.brunolima.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		
+//		Optional<Pedido> obj = repo.findById(id);
+//		return obj.orElse(null);
+	}
+
+}
